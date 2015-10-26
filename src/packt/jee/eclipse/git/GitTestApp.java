04e489b5b7e46df9eab06cb2ba4cec9adc9b7eaf
chedekun <chedekun@gmail.com>
2015-10-26 19:17:23 +0800

    Add New.jsp files

---
+++ src/packt/jee/eclipse/git/GitTestApp.java
@@ -0,0 +1,12 @@
+package packt.jee.eclipse.git;
+
+public class GitTestApp
+{
+
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+		System.out.println("Hello,Git ");
+	}
+
+}
